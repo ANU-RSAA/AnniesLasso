a2e61e5daec417e3494b0884f3e6537bdc572b1e
Marc White <mwhite1206@gmail.com>
2024-10-11 16:54:56 +1100

    Complete Censors init testing

--- thecannon/tests/test_censoring.py
+++ thecannon/tests/test_censoring.py
@@ -2,6 +2,7 @@
 Unit tests for `thecannon.censoring`.
 """
 import pytest
+import numpy as np
 
 from ..censoring import Censors
 
@@ -10,6 +11,10 @@ from ..censoring import Censors
 @pytest.mark.parametrize("label_names", [("a", "b", "c", 2, 3, 4),])
 @pytest.mark.parametrize("num_pixels", [2, 4, 6, 8, 10, 100, 1000, 10000])
 def test_censors_init(label_names, num_pixels):
-    c = Censors(label_names, num_pixels)
+    dummy_items = {l: np.ones(num_pixels, dtype=bool) for l in label_names}
+    c = Censors(label_names, num_pixels, items=dummy_items)
     assert c._label_names == label_names
     assert c._num_pixels == num_pixels
+    assert c.keys() == dummy_items.keys()
+    for k in c.keys():
+        assert np.all(c[k] == dummy_items[k])
