8b06e07b1096739d95ba036b692fc5ea9c8e59e9
Marc White <mwhite1206@gmail.com>
2025-02-27 13:08:59 +1100

    Re-write existing utils test in pytest form

--- thecannon/tests/test_utils.py
+++ thecannon/tests/test_utils.py
@@ -5,18 +5,13 @@
 Unit tests for general utility functions.
 """
 
-import unittest
-from .. import utils
+import pytest
 
+from thecannon import utils
 
-class TestShortHash(unittest.TestCase):
+def test_short_hash_different_boolean():
+    assert utils.short_hash(True) != utils.short_hash(False), "hash match for True and False"
 
-    def test_different(self):
-        self.assertNotEqual(utils.short_hash(True), utils.short_hash(False))
-
-    def test_hashing(self):
-        # So that it does not change with time and break old hashes.
-        self.assertEqual(
-            "7fc56270e79d5ed678fe0d61f8370cf623e75af33a3ea00cfc",
-            utils.short_hash("ABCDE"),
-        )
+def test_hash_consistency():
+    """Ensure hashing does not change with time and break old hashes."""
+    assert utils.short_hash("ABCDE") == "7fc56270e79d5ed678fe0d61f8370cf623e75af33a3ea00cfc"
