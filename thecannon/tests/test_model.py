41326f3476be65011850f87b083689e86fecbd08
Marc White <mwhite1206@gmail.com>
2025-02-28 14:47:28 +1100

    Test bad vectorizer input

--- thecannon/tests/test_model.py
+++ thecannon/tests/test_model.py
@@ -9,6 +9,7 @@ from unittest import mock
 
 import numpy as np
 
+
 @pytest.mark.parametrize("trained", [True, False])
 def test_requires_training_decorator(trained):
     class TestObject(object):
@@ -29,6 +30,23 @@ def test_requires_training_decorator(trained):
 
 
 @pytest.mark.parametrize(
+    "vec_bad",
+    [
+        None,
+        "a",
+        1,
+        1.0,
+        13.5,
+        "BaseVectorizer",
+        BaseVectorizer,  # class, not instance
+    ],
+)
+def test_cannonmodel_vectorizer_bad(vec_bad):
+    with pytest.raises(TypeError):
+        _ = model.CannonModel(np.ones(10), np.ones(10), np.ones(10), vec_bad)
+
+
+@pytest.mark.parametrize(
     "vectorizer",
     [
         BaseVectorizer,
@@ -43,21 +61,21 @@ def test_requires_training_decorator(trained):
     ],
 )
 class TestCannonModelInit:
-
     @pytest.mark.parametrize(
-        "training_set_flux_shape,training_set_ivar_shape,training_set_labels_shape,error_match", [
-            ((10, ), (15, ), (15, ), "flux and inverse"),
-            ((15, ), (10, ), (15, ), "flux and inverse"),
-            ((15, 2), (15, 3), (15, ), "flux and inverse"),
-            ((10, ), (15, ), None, "flux and inverse"),
-            ((15, ), (10, ), None, "flux and inverse"),
+        "training_set_flux_shape,training_set_ivar_shape,training_set_labels_shape,error_match",
+        [
+            ((10,), (15,), (15,), "flux and inverse"),
+            ((15,), (10,), (15,), "flux and inverse"),
+            ((15, 2), (15, 3), (15,), "flux and inverse"),
+            ((10,), (15,), None, "flux and inverse"),
+            ((15,), (10,), None, "flux and inverse"),
             ((15, 2), (15, 3), None, "flux and inverse"),
-            ((10, ), (10, ), (15, ), "Unable to rectify"),
-            ((15, 3), None, (15, ), "both be None"),
-            (None, (15, 3), (15,3), "both be None"),
+            ((10,), (10,), (15,), "Unable to rectify"),
+            ((15, 3), None, (15,), "both be None"),
+            (None, (15, 3), (15, 3), "both be None"),
             ((15, 3), None, None, "both be None"),
             (None, (15, 3), None, "both be None"),
-        ]
+        ],
     )
     def test_cannonmodel_init_mismatched_training_sets(
         self,
@@ -67,36 +85,63 @@ class TestCannonModelInit:
         training_set_flux_shape,
         training_set_ivar_shape,
         training_set_labels_shape,
-        error_match
+        error_match,
     ):
-        vec = vectorizer(label_names=label_names, terms=terms) 
-        training_set_flux = np.ones(training_set_flux_shape) if training_set_flux_shape is not None else None
-        training_set_ivar = np.ones(training_set_ivar_shape) if training_set_ivar_shape is not None else None
-        training_set_labels = np.ones(training_set_labels_shape) if training_set_labels_shape is not None else None
+        vec = vectorizer(label_names=label_names, terms=terms)
+        training_set_flux = (
+            np.ones(training_set_flux_shape)
+            if training_set_flux_shape is not None
+            else None
+        )
+        training_set_ivar = (
+            np.ones(training_set_ivar_shape)
+            if training_set_ivar_shape is not None
+            else None
+        )
+        training_set_labels = (
+            np.ones(training_set_labels_shape)
+            if training_set_labels_shape is not None
+            else None
+        )
 
         with pytest.raises(ValueError, match=error_match):
-            _ = model.CannonModel(training_set_labels, training_set_flux, training_set_ivar, vec)
+            _ = model.CannonModel(
+                training_set_labels, training_set_flux, training_set_ivar, vec
+            )
 
-    @pytest.mark.parametrize("training_set_shape", [(10,), (10, 10), (100, ), (100, 100), (10, 100)])
+    @pytest.mark.parametrize(
+        "training_set_shape", [(10,), (10, 10), (100,), (100, 100), (10, 100)]
+    )
     @pytest.mark.parametrize("inf_posn", [0, 25, 50, 75, 99])
     class TestCannonModelInfValues(object):
-
-        def test_cannonmodel_inf_flux(self, vectorizer, label_names, terms, training_set_shape, inf_posn):
+        def test_cannonmodel_inf_flux(
+            self, vectorizer, label_names, terms, training_set_shape, inf_posn
+        ):
             vec = vectorizer(label_names=label_names, terms=terms)
             training_set_flux = np.ones(training_set_shape)
-            training_set_flux[int((inf_posn/100.0) * training_set_flux.shape[0])] = np.inf
+            training_set_flux[
+                int((inf_posn / 100.0) * training_set_flux.shape[0])
+            ] = np.inf
             training_set_ivar = np.ones(training_set_shape)
             training_set_labels = np.ones(training_set_shape)
 
             with pytest.raises(ValueError, match="fluxes.*finite"):
-                _ = model.CannonModel(training_set_labels, training_set_flux, training_set_ivar, vec)
+                _ = model.CannonModel(
+                    training_set_labels, training_set_flux, training_set_ivar, vec
+                )
 
-        def test_cannonmodel_inf_ivar(self, vectorizer, label_names, terms, training_set_shape, inf_posn):
+        def test_cannonmodel_inf_ivar(
+            self, vectorizer, label_names, terms, training_set_shape, inf_posn
+        ):
             vec = vectorizer(label_names=label_names, terms=terms)
             training_set_flux = np.ones(training_set_shape)
             training_set_ivar = np.ones(training_set_shape)
-            training_set_ivar[int((inf_posn/100.0) * training_set_ivar.shape[0])] = np.inf
+            training_set_ivar[
+                int((inf_posn / 100.0) * training_set_ivar.shape[0])
+            ] = np.inf
             training_set_labels = np.ones(training_set_shape)
 
             with pytest.raises(ValueError, match="ivars.*finite"):
-                _ = model.CannonModel(training_set_labels, training_set_flux, training_set_ivar, vec)
+                _ = model.CannonModel(
+                    training_set_labels, training_set_flux, training_set_ivar, vec
+                )
