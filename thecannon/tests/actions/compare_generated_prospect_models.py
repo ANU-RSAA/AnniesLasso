4750a934261b86f668f7355cd2de13b690db876d
Marc White <mwhite1206@gmail.com>
2025-06-06 17:18:48 +1000

    Better reporting in compare_generated_prospect_models

--- thecannon/tests/actions/compare_generated_prospect_models.py
+++ thecannon/tests/actions/compare_generated_prospect_models.py
@@ -12,10 +12,29 @@ assert old_model == new_model, "Model mismatch!"
 orig_labels = "thecannon/tests/data/prospect_model_labels_ref.npy"
 new_labels = "prospect_model_labels.npy"
 
-assert np.allclose(np.load(orig_labels), np.load(new_labels)), "Test labels mismatch!"
+orig_labels_arr = np.load(orig_labels)
+new_labels_arr = np.load(new_labels)
+
+try:
+    assert np.allclose(orig_labels_arr, new_labels_arr)
+except AssertionError:
+    labels_delta = new_labels_arr - orig_labels_arr
+    raise AssertionError(
+        f"Test labels mismatch: delta stats mean={np.mean(np.abs(labels_delta))}, "
+        f"median={np.median(labels_delta)}, std={np.std(labels_delta)}"
+    )
 
 
 orig_cov = "thecannon/tests/data/prospect_model_cov_ref.npy"
 new_cov = "prospect_model_cov.npy"
 
-assert np.allclose(np.load(orig_cov), np.load(new_cov)), "Covariance mismatch!"
+orig_cov_arr = np.load(orig_labels)
+new_cov_arr = np.load(new_labels)
+try:
+    assert np.allclose(orig_cov_arr, new_cov_arr)
+except AssertionError:
+    cov_delta = new_cov_arr - old_cov_arr
+    raise AssertionError(
+        f"Test cov mismatch: delta stats: (abs)mean={np.mean(np.abs(cov_delta))}, "
+        f"median={np.median(cov_delta)}, std={np.std(cov_delta)}"
+    )
