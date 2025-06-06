53416edaef60c2fb75c9f2f316088c9da0d6dbbb
Marc White <mwhite1206@gmail.com>
2025-06-06 16:57:37 +1000

    Add test label, cov checking to test_model_output

--- thecannon/tests/actions/compare_generated_prospect_models.py
+++ thecannon/tests/actions/compare_generated_prospect_models.py
@@ -1,4 +1,5 @@
 from thecannon.model import CannonModel
+import numpy as np
 
 orig = "thecannon/tests/data/prospect_model_ref.model"
 new = "prospect_model_test.model"
@@ -7,3 +8,14 @@ old_model = CannonModel.read(orig)
 new_model = CannonModel.read(new)
 
 assert old_model == new_model, "Model mismatch!"
+
+orig_labels = "thecannon/tests/data/prospect_model_labels_ref.npy"
+new_labels = "prospect_model_labels.npy"
+
+assert np.allclose(np.load(orig_labels), np.load(new_labels)), "Test labels mismatch!"
+
+
+orig_cov = "thecannon/tests/data/prospect_model_cov_ref.npy"
+new_cov = "prospect_model_cov.npy"
+
+assert np.allclose(np.load(orig_cov), np.load(new_cov)), "Covariance mismatch!"
