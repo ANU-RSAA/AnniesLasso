21a062cf3b3cd5fb9b5d637b0180a6021841dc27
Marc White <marc.white@anu.edu.au>
2025-07-29 15:36:58 +1000

    Add test step label & covariance checking to end to end test (`test_model_output`) action (#25)
    * Add test label, cov checking to test_model_output
    
    * Update gitignore
    
    * Remove stray committed files
    
    * Better reporting in compare_generated_prospect_models
    
    * Fix typo
    
    * Add max diff reporting
    
    * Try setting an atol
    
    * Try out an rtol-only scheme
    
    * Add report of values at max diff
    
    * Fix reporting line
    
    * Update labels print format for fail message
    
    * Correct f string formatting
    
    * Alter rtol to 1e-6 (i.e. 7 sig figs)
    
    * Increase rtol to 1e-5 (6 sig figs)
    
    * Loosen right off as a test
    
    * Match subtraction directions between allclose and debug statements
    
    * Getting closer
    
    * Push it further
    
    * Went too far

--- thecannon/tests/actions/compare_generated_prospect_models.py
+++ thecannon/tests/actions/compare_generated_prospect_models.py
@@ -1,4 +1,5 @@
 from thecannon.model import CannonModel
+import numpy as np
 
 orig = "thecannon/tests/data/prospect_model_ref.model"
 new = "prospect_model_test.model"
@@ -7,3 +8,35 @@ old_model = CannonModel.read(orig)
 new_model = CannonModel.read(new)
 
 assert old_model == new_model, "Model mismatch!"
+
+orig_labels = "thecannon/tests/data/prospect_model_labels_ref.npy"
+new_labels = "prospect_model_labels.npy"
+
+orig_labels_arr = np.load(orig_labels)
+new_labels_arr = np.load(new_labels)
+
+try:
+    assert np.allclose(orig_labels_arr, new_labels_arr, atol=0, rtol=1e-4)
+except AssertionError:
+    labels_delta = orig_labels_arr - new_labels_arr
+    raise AssertionError(
+        f"Test labels mismatch: delta stats mean={np.mean(np.abs(labels_delta))}, "
+        f"median={np.median(labels_delta)}, std={np.std(labels_delta)}, "
+        f"max={np.max(labels_delta)} @ {(orig_labels_arr.flatten())[np.argmax(labels_delta)]:.9e}, {(new_labels_arr.flatten())[np.argmax(labels_delta)]:.9e}"
+    )
+
+
+orig_cov = "thecannon/tests/data/prospect_model_cov_ref.npy"
+new_cov = "prospect_model_cov.npy"
+
+orig_cov_arr = np.load(orig_labels)
+new_cov_arr = np.load(new_labels)
+try:
+    assert np.allclose(orig_cov_arr, new_cov_arr, atol=0, rtol=1e-4)
+except AssertionError:
+    cov_delta = orig_cov_arr - new_cov_arr
+    raise AssertionError(
+        f"Test cov mismatch: delta stats: (abs)mean={np.mean(np.abs(cov_delta))}, "
+        f"median={np.median(cov_delta)}, std={np.std(cov_delta)}, "
+        f"max={np.abs(cov_delta)} @ {(orig_cov.flatten)[np.argmax(cov_delta)]:.9e}, {(new_cov.flatten())[np.argmax(cov_delta)]:.9e}"
+    )
