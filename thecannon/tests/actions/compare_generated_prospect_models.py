a95ac79ace401177f5b6ac69ab0d21282dbcfc85
Marc White <mwhite1206@gmail.com>
2025-10-20 13:24:29 +1100

    Add label value printout to Github action

--- thecannon/tests/actions/compare_generated_prospect_models.py
+++ thecannon/tests/actions/compare_generated_prospect_models.py
@@ -22,7 +22,9 @@ except AssertionError:
     raise AssertionError(
         f"Test labels mismatch: delta stats mean={np.mean(np.abs(labels_delta))}, "
         f"median={np.median(labels_delta)}, std={np.std(labels_delta)}, "
-        f"max={np.max(labels_delta)} @ {(orig_labels_arr.flatten())[np.argmax(labels_delta)]:.9e}, {(new_labels_arr.flatten())[np.argmax(labels_delta)]:.9e}"
+        f"max={np.max(labels_delta)} @ {(orig_labels_arr.flatten())[np.argmax(labels_delta)]:.9e}, {(new_labels_arr.flatten())[np.argmax(labels_delta)]:.9e}\n"
+        f"Original labels: {orig_labels_arr}\n",
+        f"New labels: {new_labels_arr}"
     )
 
 
